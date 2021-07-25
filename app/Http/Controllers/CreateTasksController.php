35f42ee58b58da27b3a513c40613717a779ed840
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-07-25 09:29:50 -0300

    ✨ NEW FEATURE: create tasks controler

--- app/Http/Controllers/CreateTasksController.php
+++ app/Http/Controllers/CreateTasksController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class CreateTasksController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        return "OK";
+        $task = Task::create([
+            'name' => $request->input('name'),
+            'completed' => $request->input('completed')
+        ]);
+
+        return $task;
     }
 }
