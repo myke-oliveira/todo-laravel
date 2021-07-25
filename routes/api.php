0fa56dc6aa1529510205b883f19bc6f18bc33888
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-07-25 09:31:08 -0300

    🔨 REFACTOR: rename CreateTasksController to CreateTaskController

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListTasksController;
-use App\Http\Controllers\CreateTasksController;
+use App\Http\Controllers\CreateTaskController;
 
 Route::get('tasks/', ListTasksController::class);
-Route::post('tasks/', CreateTasksController::class);
+Route::post('tasks/', CreateTaskController::class);
