fca74aee67ae7155269c4fd063a8b9d165ad1d69
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-07-25 11:12:12 -0300

    ✨ NEW FEATURE: implement edit task

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListTasksController;
 use App\Http\Controllers\GetTaskController;
 use App\Http\Controllers\CreateTaskController;
+use App\Http\Controllers\EditTaskController;
 
 Route::get('tasks/', ListTasksController::class);
 Route::get('tasks/{id}', GetTaskController::class);
 Route::post('tasks/', CreateTaskController::class);
+Route::patch('tasks/{id}', EditTaskController::class);
