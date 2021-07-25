2af4c01d52427db9215f6ddd878fc47a96afebaa
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-07-25 09:29:25 -0300

    🔨 REFACTOR: Rename Tasks model to Task

--- app/Http/Controllers/ListTasksController.php
+++ app/Http/Controllers/ListTasksController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Tasks;
+use App\Models\Task;
 
 class ListTasksController extends Controller
 {
     public function __invoke()
     {
-        return Tasks::all();
+        return Task::all();
     }
 }
