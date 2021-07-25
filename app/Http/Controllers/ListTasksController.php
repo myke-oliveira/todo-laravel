b32fa3766565f15bb5232f4101aa242451f9c25c
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-07-25 09:13:50 -0300

    ✨ NEW FEATURE: Tasks from database.

--- app/Http/Controllers/ListTasksController.php
+++ app/Http/Controllers/ListTasksController.php
@@ -3,27 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tasks;
 
 class ListTasksController extends Controller
 {
     public function __invoke()
     {
-        return [
-            [
-                'id' => 1,
-                'name' => 'Estudar PHP 7',
-                'completed' => false,
-            ],
-            [
-                'id' => 2,
-                'name' => 'Estudar JavaScript',
-                'completed' => true,
-            ],
-            [
-                'id' => 3,
-                'name' => 'Estudar Laravel',
-                'completed' => false,
-            ],
-        ];
+        return Tasks::all();
     }
 }
